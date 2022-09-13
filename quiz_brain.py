b266eaf142811390bec8fb7bb695b02a8c64c164
thulanimbatha <thulanimbatha06@gmail.com>
2022-09-14 00:46:19 +0200

    could not solve bug, IndexError

--- quiz_brain.py
+++ quiz_brain.py
@@ -9,15 +9,13 @@ class QuizBrain:
         self.current_question = None
 
     def still_has_questions(self):
-        return self.question_number < len(self.question_list)
+        return self.question_number < len(self.question_list) 
 
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         question_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {question_text} (True/False): "
-        # user_answer = input(f"Q.{self.question_number}: {question_text} (True/False): ")
-        # self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from tkinter import *
 from quiz_brain import QuizBrain
 
@@ -35,9 +36,14 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        self.score_label.config(text=f"Score: {self.quiz.score}")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions:
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You ran out of questions")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_pressed(self):
         is_right = self.quiz.check_answer("True")
